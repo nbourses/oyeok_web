d41d3007ec7719daa1353aa77ec207daa5f78f57
Mayuresh Gomai <gomaimayuresh@gmail.com>
2015-10-14 20:24:00 +0530

    changed the server side url /user/web to /web/oye

--- hail.php
+++ hail.php
@@ -16,7 +16,7 @@
 	$long = $_POST['long'];
 	$mob_no = $_POST['phone_no'];
 	$json = array('mobile_no'=>$mob_no,'long'=>$long,'lat'=>$lat,'spec_code'=>"$property-$intend-$size-$budget");
-	$myCurl = "curl -i 52.25.136.179:9000/1/user/web -d '". json_encode($json)."' -H 'Content-Type: application/json'";
+	$myCurl = "curl -i 52.25.136.179:9000/1/web/oye -d '". json_encode($json)."' -H 'Content-Type: application/json'";
 	echo $myCurl;
 	$output = shell_exec($myCurl);
 	echo "<pre>$output</pre>";
